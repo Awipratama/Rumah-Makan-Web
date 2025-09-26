4ba4b260544d643143f81bdd82d2782ee567e30b
Awipratama <awipratama05122006@gmail.com>
2025-09-26 15:17:45 +0800

    tailwind-setup

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,7 +1,27 @@
+import Logo from '../assets/Logo.png';
+
 function Navigation() {
     return (
         <nav>
-
+            <ul>
+                <li>
+                    <a href="#">Menus</a>
+                </li>
+                <li>
+                    <a href="#">Orders</a>
+                </li>
+                <li>
+                    <a href="#">
+                        <img src={Logo} alt="Logo" />
+                    </a>
+                </li>
+                <li>
+                    <a href="#">Testimoni</a>
+                </li>
+                <li>
+                    <a href="#">Contact</a>
+                </li>
+            </ul>
         </nav>
     )
 }
